934df76e9ba66ce6afd003c63e4aa5c9ef377830
sitek94 <msitkowski94@gmail.com>
2021-03-04 16:59:23 +0100

    remove test code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import printMe from './print';
-
 function component() {
   const element = document.createElement('div');
-  const btn = document.createElement('button');
 
   element.innerHTML = `Hello webpack`;
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
 
   return element;
 }

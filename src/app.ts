6cf426994e36f7df15230a5c6949f0440f845cbd
sitek94 <msitkowski94@gmail.com>
2021-03-05 19:03:32 +0100

    add app settings

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+export const app = {
+  pointsForCorrect: 10,
+  pointsForIncorrect: 5,
+  msTimeoutAfterCorrect: 1500,
+  msTimeoutAfterIncorrect: 1000,
+  gameLengthInSeconds: 20,
+  bonusSecondsForCorrectPerCoin: 1,
+  maxHighScoreItems: 3,
+  starterHighScoreList: [
+    { name: 'Alice', score: 29 },
+    { name: 'Bob', score: 19 },
+    { name: 'Charlie', score: 9 },
+  ],
+};

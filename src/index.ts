e34d47e52691c640855e69fa7c58434cf149e172
sitek94 <msitkowski94@gmail.com>
2021-03-05 15:15:38 +0100

    test modal works

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import * as ko from 'knockout';
+import * as $ from 'jquery';
+
 import { CoinCounter } from './CoinCounter';
 
+$('#myBtn').on('click', () => {
+  $('#myModal').modal('toggle');
+});
 
 ko.applyBindings(new CoinCounter(), document.querySelector('#app'));
